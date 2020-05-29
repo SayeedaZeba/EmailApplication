1af906e9067d61b6da44aa3e5591823d17811f95
Zeba Sayeeda <31102968+SayeedaZeba@users.noreply.github.com>
2020-05-29 01:32:42 -0400

    class Email

--- Email.java
+++ Email.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package emailapp;
 import java.util.Scanner;
 
-/**
- *
- * @author Iffath Zeba Sayeeda
- */
 public class Email {
     private String firstName;
     private String lastName;
