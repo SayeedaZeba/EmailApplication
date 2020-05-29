0d388dacfd2b8eb7ba3485fbb91db032ec3ab680
Zeba Sayeeda <31102968+SayeedaZeba@users.noreply.github.com>
2020-05-29 01:33:40 -0400

    Email Application

--- EmailApp.java
+++ EmailApp.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package emailapp;
 
-/**
- *
- * @author Iffath Zeba Sayeeda
- */
 public class EmailApp {
     
     
     public static void main(String[] args){
-        Email e1 = new Email("Bilz","Cutie");     
+        Email e1 = new Email("FirstName","LastName");     
         System.out.println(e1.showInfo());
     }
     
